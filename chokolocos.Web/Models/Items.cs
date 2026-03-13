d2d252c33a42dc038a332e826aec5e63ea596b7d
Angel Garza <agarza@aph.mx>
2026-03-13 12:35:36 -0600

    Secciones de redes sociales

--- chokolocos.Web/Models/Items.cs
+++ chokolocos.Web/Models/Items.cs
@@ -5,4 +5,5 @@ public class Item
     public int Id { get; set; }
     public required string Name { get; set; }
     public required List<string> Pictures { get; set; } // Lista de URLs de imágenes
+    public required string Link { get; set; }
 }
